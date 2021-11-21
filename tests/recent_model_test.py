ea60780ec79dc45adb97e6d1b36e7b632bc9fc6d
Preocts <preocts@gmail.com>
2021-11-21 15:05:13 -0500

    Rename fixture file

--- tests/recent_model_test.py
+++ tests/recent_model_test.py
@@ -3,7 +3,7 @@ import json
 from twitterapiv2.model.base_model import BaseModel
 from twitterapiv2.model.recent.recent import Recent
 
-MOCK_MODEL = json.load(open("tests/fixtures/mock_model.json", "r"))
+MOCK_MODEL = json.load(open("tests/fixtures/mock_recent.json", "r"))
 
 
 def test_apply_model() -> None:
