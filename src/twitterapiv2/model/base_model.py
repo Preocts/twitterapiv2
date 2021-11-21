dae571a74d1fdc05bd4d776862174d8d7f95b141
Preocts <preocts@gmail.com>
2021-11-21 02:30:41 -0500

    Create repl and to_json methods

--- src/twitterapiv2/model/base_model.py
+++ src/twitterapiv2/model/base_model.py
@@ -1,10 +1,23 @@
+import json
 from abc import ABC
 from typing import Any
 from typing import Dict
 
 
+class BaseModelEncoder(json.JSONEncoder):
+    def default(self, o: Any) -> Any:
+        return o.__dict__
+
+
 class BaseModel(ABC):
     """An empty model object"""
+
+    def __repr__(self) -> str:
+        return json.dumps(self, cls=BaseModelEncoder)
+
+    def to_json(self) -> Dict[str, Any]:
+        """Returns objects as serialized dictionary (JSON)"""
+        return json.loads(self.__repr__())
 
     def build_from(self, data: Dict[str, Any]) -> "BaseModel":
         raise NotImplementedError  # pragma: no cover
