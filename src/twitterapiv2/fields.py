3a1ebda1877946e9b7dc350a926580ef7be20bbd
Preocts <preocts@gmail.com>
2021-11-23 23:02:30 -0500

    Refactor fields into builder class

---
+++ src/twitterapiv2/fields.py
@@ -0,0 +1,105 @@
+from datetime import datetime
+from typing import Any
+from typing import Dict
+from typing import Optional
+from typing import Union
+
+from twitterapiv2.util.rules import is_ISO8601
+from twitterapiv2.util.rules import to_ISO8601
+
+
+class Fields:
+    def __init__(self) -> None:
+        self._fields: Dict[str, Any] = {}
+
+    @property
+    def fields(self) -> Dict[str, Any]:
+        """Returns fields that have been defined"""
+        return {key: str(value) for key, value in self._fields.items() if value}
+
+    def start_time(self, start: Union[str, datetime, None]) -> None:
+        """Define start_time of query. YYYY-MM-DDTHH:mm:ssZ (ISO 8601/RFC 3339)"""
+        if isinstance(start, datetime):
+            start = to_ISO8601(start)
+        elif start is not None and not is_ISO8601(start):
+            raise ValueError("Datetime format expected: 'YYYY-MM-DDTHH:mm:ssZ'")
+        self._fields["start_time"] = start
+
+    def end_time(self, end: Union[str, datetime, None]) -> None:
+        """
+        Define end_time of query. YYYY-MM-DDTHH:mm:ssZ (ISO 8601/RFC 3339)
+
+        NOTE: The end_time cannot be less than 10 seconds from "now"
+        """
+        if isinstance(end, datetime):
+            end = to_ISO8601(end)
+        elif end is not None and not is_ISO8601(end):
+            raise ValueError("Datetime format expected: 'YYYY-MM-DDTHH:mm:ssZ'")
+        self._fields["end_time"] = end
+
+    def since_id(self, since_id: Optional[str]) -> None:
+        """Define since_id of query. Returns results with a Tweet ID greater than"""
+        self._fields["since_id"] = since_id if since_id else None
+
+    def until_id(self, until_id: Optional[str]) -> None:
+        """Define until_id of query. Returns results with a Tweet ID less than"""
+        self._fields["until_id"] = until_id if until_id else None
+
+    def expansions(self, expansions: Optional[str]) -> None:
+        """
+        Define expansions of query. Comma seperated with no spaces:
+            attachments.poll_ids, attachments.media_keys, author_id,
+            entities.mentions.username, geo.place_id, in_reply_to_user_id,
+            referenced_tweets.id, referenced_tweets.id.author_id
+        """
+        self._fields["expansions"] = expansions if expansions else None
+
+    def media_fields(self, media_fields: Optional[str]) -> None:
+        """
+        Define media_fields of query. Comma seperated with no spaces:
+            duration_ms, height, media_key, preview_image_url,
+            type, url, width, public_metrics, non_public_metrics,
+            organic_metrics, promoted_metrics, alt_text
+        """
+        self._fields["media.fields"] = media_fields if media_fields else None
+
+    def place_fields(self, place_fields: Optional[str]) -> None:
+        """
+        Define place_fields of query. Comma seperated with no spaces:
+            contained_within, country, country_code, full_name,
+            geo, id, name, place_type
+        """
+        self._fields["place.fields"] = place_fields if place_fields else None
+
+    def poll_fields(self, poll_fields: Optional[str]) -> None:
+        """
+        Define poll_fields of query. Comma seperated with no spaces:
+            duration_minutes, end_datetime, id, options, voting_status
+        """
+        self._fields["poll.fields"] = poll_fields if poll_fields else None
+
+    def tweet_fields(self, tweet_fields: Optional[str]) -> None:
+        """
+        Define tweet_fields of query. Comma seperated with no spaces:
+            attachments, author_id, context_annotations, conversation_id,
+            created_at, entities, geo, id, in_reply_to_user_id, lang,
+            non_public_metrics, public_metrics, organic_metrics,
+            promoted_metrics, possibly_sensitive, referenced_tweets,
+            reply_settings, source, text, withheld
+        """
+        self._fields["tweet.fields"] = tweet_fields if tweet_fields else None
+
+    def user_fields(self, user_fields: Optional[str]) -> None:
+        """
+        Define user_fields of query. Comma seperated with no spaces:
+            created_at, description, entities, id, location, name,
+            pinned_tweet_id, profile_image_url, protected,
+            public_metrics, url, username, verified, withheld
+        """
+        self._fields["user.fields"] = user_fields if user_fields else None
+
+    def max_results(self, max_results: Optional[int]) -> None:
+        """A number between 10 and 100. By default, set at 10 results"""
+        if max_results is not None and max_results not in range(10, 101):
+            raise ValueError("max_results must be between 10 and 100")
+        self._fields["max_results"] = max_results if max_results else None
--- src/twitterapiv2/search_recent.py
+++ src/twitterapiv2/search_recent.py
@@ -1,13 +1,10 @@
-from datetime import datetime
 from typing import Any
 from typing import Dict
 from typing import Optional
-from typing import Union
 
+from twitterapiv2.fields import Fields
 from twitterapiv2.http import Http
 from twitterapiv2.model.recent.recent import Recent
-from twitterapiv2.util.rules import is_ISO8601
-from twitterapiv2.util.rules import to_ISO8601
 
 
 class SearchRecent:
@@ -22,117 +19,30 @@ class SearchRecent:
         applicaton bearer token. This can be defined manually or loaded with the
         use of AuthClient.set_bearer_token().
         """
+        self.field_builder = Fields()
         self.http = Http()
-        self._fields: Dict[str, Any] = {}
         self._next_token: Optional[str] = None
+        self.start_time = self.field_builder.start_time
+        self.end_time = self.field_builder.end_time
+        self.since_id = self.field_builder.since_id
+        self.until_id = self.field_builder.until_id
+        self.expansions = self.field_builder.expansions
+        self.media_fields = self.field_builder.media_fields
+        self.place_fields = self.field_builder.place_fields
+        self.poll_fields = self.field_builder.poll_fields
+        self.tweet_fields = self.field_builder.tweet_fields
+        self.user_fields = self.field_builder.user_fields
+        self.max_results = self.field_builder.max_results
 
     @property
     def fields(self) -> Dict[str, Any]:
-        """Returns fields that have been defined"""
-        return {key: str(value) for key, value in self._fields.items() if value}
+        """..."""
+        return self.field_builder.fields
 
     @property
     def next_token(self) -> Optional[str]:
         """Return next_token for pagination or `None` when all results are polled"""
         return self._next_token
-
-    def start_time(self, start: Union[str, datetime, None]) -> "SearchRecent":
-        """Define start_time of query. YYYY-MM-DDTHH:mm:ssZ (ISO 8601/RFC 3339)"""
-        if isinstance(start, datetime):
-            start = to_ISO8601(start)
-        elif start is not None and not is_ISO8601(start):
-            raise ValueError("Datetime format expected: 'YYYY-MM-DDTHH:mm:ssZ'")
-        self._fields["start_time"] = start
-        return self._new_client()
-
-    def end_time(self, end: Union[str, datetime, None]) -> "SearchRecent":
-        """
-        Define end_time of query. YYYY-MM-DDTHH:mm:ssZ (ISO 8601/RFC 3339)
-
-        NOTE: The end_time cannot be less than 10 seconds from "now"
-        """
-        if isinstance(end, datetime):
-            end = to_ISO8601(end)
-        elif end is not None and not is_ISO8601(end):
-            raise ValueError("Datetime format expected: 'YYYY-MM-DDTHH:mm:ssZ'")
-        self._fields["end_time"] = end
-        return self._new_client()
-
-    def since_id(self, since_id: Optional[str]) -> "SearchRecent":
-        """Define since_id of query. Returns results with a Tweet ID greater than"""
-        self._fields["since_id"] = since_id if since_id else None
-        return self._new_client()
-
-    def until_id(self, until_id: Optional[str]) -> "SearchRecent":
-        """Define until_id of query. Returns results with a Tweet ID less than"""
-        self._fields["until_id"] = until_id if until_id else None
-        return self._new_client()
-
-    def expansions(self, expansions: Optional[str]) -> "SearchRecent":
-        """
-        Define expansions of query. Comma seperated with no spaces:
-            attachments.poll_ids, attachments.media_keys, author_id,
-            entities.mentions.username, geo.place_id, in_reply_to_user_id,
-            referenced_tweets.id, referenced_tweets.id.author_id
-        """
-        self._fields["expansions"] = expansions if expansions else None
-        return self._new_client()
-
-    def media_fields(self, media_fields: Optional[str]) -> "SearchRecent":
-        """
-        Define media_fields of query. Comma seperated with no spaces:
-            duration_ms, height, media_key, preview_image_url,
-            type, url, width, public_metrics, non_public_metrics,
-            organic_metrics, promoted_metrics, alt_text
-        """
-        self._fields["media.fields"] = media_fields if media_fields else None
-        return self._new_client()
-
-    def place_fields(self, place_fields: Optional[str]) -> "SearchRecent":
-        """
-        Define place_fields of query. Comma seperated with no spaces:
-            contained_within, country, country_code, full_name,
-            geo, id, name, place_type
-        """
-        self._fields["place.fields"] = place_fields if place_fields else None
-        return self._new_client()
-
-    def poll_fields(self, poll_fields: Optional[str]) -> "SearchRecent":
-        """
-        Define poll_fields of query. Comma seperated with no spaces:
-            duration_minutes, end_datetime, id, options, voting_status
-        """
-        self._fields["poll.fields"] = poll_fields if poll_fields else None
-        return self._new_client()
-
-    def tweet_fields(self, tweet_fields: Optional[str]) -> "SearchRecent":
-        """
-        Define tweet_fields of query. Comma seperated with no spaces:
-            attachments, author_id, context_annotations, conversation_id,
-            created_at, entities, geo, id, in_reply_to_user_id, lang,
-            non_public_metrics, public_metrics, organic_metrics,
-            promoted_metrics, possibly_sensitive, referenced_tweets,
-            reply_settings, source, text, withheld
-        """
-        self._fields["tweet.fields"] = tweet_fields if tweet_fields else None
-        return self._new_client()
-
-    def user_fields(self, user_fields: Optional[str]) -> "SearchRecent":
-        """
-        Define user_fields of query. Comma seperated with no spaces:
-            created_at, description, entities, id, location, name,
-            pinned_tweet_id, profile_image_url, protected,
-            public_metrics, url, username, verified, withheld
-        """
-        self._fields["user.fields"] = user_fields if user_fields else None
-        return self._new_client()
-
-    def max_results(self, max_results: Optional[int]) -> "SearchRecent":
-        """A number between 10 and 100. By default, set at 10 results"""
-        if max_results is not None and max_results not in range(10, 101):
-            raise ValueError("max_results must be between 10 and 100")
-        self._fields["max_results"] = max_results if max_results else None
-        return self._new_client()
 
     def fetch(
         self,
@@ -147,14 +57,8 @@ class SearchRecent:
         `next_token` parameter. These default to None and can be
         safely referenced prior to, and after, searches.
         """
-        self._fields["query"] = query
-        self._fields["next_token"] = page_token
+        self.field_builder._fields["query"] = query
+        self.field_builder._fields["next_token"] = page_token
         result = Recent.build_from(self.http.get(self.URL, self.fields))
         self._next_token = result.meta.next_token
         return result
-
-    def _new_client(self) -> "SearchRecent":
-        """Used to create a new client with attributes carried forward"""
-        new_client = SearchRecent()
-        new_client._fields.update(self._fields)
-        return new_client
