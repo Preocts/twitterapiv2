385ec2b3a0af3640498ccafc68f56ade96653b8c
Preocts <preocts@gmail.com>
2021-11-24 23:40:17 -0500

    Create interface for clients

---
+++ src/twitterapiv2/client_intrfc.py
@@ -0,0 +1,37 @@
+from typing import Any
+from typing import Dict
+from typing import Optional
+
+from twitterapiv2.fields import Fields
+from twitterapiv2.http import Http
+
+
+class ClientIntrfc:
+    def __init__(self) -> None:
+        super().__init__()
+        self.field_builder = Fields()
+        self.http = Http()
+        self._next_token: Optional[str] = None
+
+    @property
+    def fields(self) -> Dict[str, Any]:
+        """Returns fields that have been defined (removed NoneTypes)"""
+        fields = self.field_builder.fields
+        fields["next_token"] = self._next_token
+        return {key: str(value) for key, value in fields.items() if value}
+
+    @property
+    def more(self) -> bool:
+        """True if more pages exist. Always starts as False"""
+        return bool(self._next_token)
+
+    def get(self, url: str) -> Dict[str, Any]:
+        """Sends a GET request to url with defined fields encoded into URL"""
+        result = self.http.get(url, self.fields)
+        meta = result.get("meta")
+        self._next_token = meta.get("next_token") if meta else None
+        return result
+
+    def fetch(self) -> None:
+        """Override with specific implementation"""
+        raise NotImplementedError
