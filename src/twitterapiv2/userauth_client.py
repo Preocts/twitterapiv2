16a080a814ad55065b3ae4114c821231eac276dc
Preocts <preocts@gmail.com>
2021-12-01 21:50:54 -0500

    OAuth1 signature string

--- src/twitterapiv2/userauth_client.py
+++ src/twitterapiv2/userauth_client.py
@@ -1,9 +1,12 @@
+import base64
+import hmac
 import logging
 import os
 from datetime import datetime
+from hashlib import sha1
 from secrets import token_urlsafe
 from typing import Dict
-from typing import Optional
+from typing import List
 from urllib import parse
 
 from twitterapiv2.http import Http
@@ -15,7 +18,7 @@ class UserAuthClient:
     def __init__(self, callback_http: str = "https://localhost") -> None:
         self.log = logging.getLogger(__name__)
         self.http = Http()
-        self.base_fields = {"callback_http": callback_http}
+        self.callback_http = callback_http
 
     def key_collect(self) -> Dict[str, str]:
         """Collect all keys needed for headers"""
@@ -27,7 +30,6 @@ class UserAuthClient:
         keys = {
             "oauth_consumer_key": consumer_key,
             "oauth_nonce": token_urlsafe(),
-            "oauth_signature": "",
             "oauth_signature_method": "HMAC-SHA1",
             "oauth_timestamp": f"{int(datetime.utcnow().timestamp())}",
             "oauth_token": access_token,
@@ -35,26 +37,38 @@ class UserAuthClient:
         }
         return keys
 
-    def generate_signature_base_string(
+    def generate_parameter_string(self, fields: Dict[str, str]) -> str:
+        """Generate OAuth1 parameter string"""
+        parameter_segments: List[str] = []
+        for field in sorted(fields):
+            key = parse.quote(field, safe="")
+            value = parse.quote(fields[field], safe="")
+            parameter_segments.append(f"{key}={value}")
+        return "&".join(parameter_segments)
+
+    def generate_base_string(
         self,
         method: str,
         route: str,
-        keys: Dict[str, str],
-        fields: Optional[Dict[str, str]] = None,
+        parameter_string: str,
     ) -> str:
-        """Generate Oauth1 signature base string"""
-        if fields is None:
-            fields = {}
-        fields = {**self.base_fields, **fields, **keys}
-        parameter_string = ""
-        for field in sorted(fields):
-            parameter_string += "&" if parameter_string else ""
-            parameter_string += parse.quote_plus(field)
-            parameter_string += "="
-            parameter_string += parse.quote_plus(fields[field])
-
+        """Generate OAuth1 base string"""
         base_string = f"{method.upper()}&"
-        base_string += parse.quote_plus(f"{BASE_URL}{route}") + "&"
-        base_string += parameter_string
-
+        base_string += parse.quote(f"{BASE_URL}{route}", safe="") + "&"
+        base_string += parse.quote(parameter_string, safe="")
         return base_string
+
+    def generate_signature_string(self, base_string: str) -> str:
+        """Generate OAuth1 signature"""
+        consumer_secret = os.getenv("TW_CONSUMER_SECRET", None)
+        access_secret = os.getenv("TW_ACCESS_SECRET", None)
+        if consumer_secret is None or access_secret is None:
+            raise KeyError("Missing consumer/access environment variable(s).")
+        base_bytes = base_string.encode("utf-8")
+        combined = (
+            parse.quote(consumer_secret, safe="")
+            + "&"
+            + parse.quote(access_secret, safe="")
+        ).encode("utf-8")
+        hash_bytes = hmac.new(combined, base_bytes, sha1).digest()
+        return base64.encodebytes(hash_bytes).decode("utf-8").rstrip("\n")
---
+++ tests/userauth_client_test.py
@@ -0,0 +1,94 @@
+import os
+from typing import Any
+from typing import Dict
+from unittest.mock import patch
+
+import pytest
+from twitterapiv2.userauth_client import UserAuthClient
+
+MOCK_VALUES: Dict[str, Any] = {
+    "method": "post",
+    "route": "/1.1/statuses/update.json",
+    "fields": {
+        "include_entities": "true",
+        "status": "Hello Ladies + Gentlemen, a signed OAuth request!",
+    },
+    "oauth_nonce": "kYjzVBB8Y0ZFabxSWbWovY3uYSQ2pTgmZeNu2VS4cg",
+    "oauth_timestamp": "1318622958",
+    "expected_parameter_string": (
+        "include_entities=true&oauth_consumer_key=xvz1evFS4wEEPTGEFPHBog&"
+        "oauth_nonce=kYjzVBB8Y0ZFabxSWbWovY3uYSQ2pTgmZeNu2VS4cg&"
+        "oauth_signature_method=HMAC-SHA1&oauth_timestamp=1318622958&"
+        "oauth_token=370773112-GmHxMAgYyLbNEtIKZeRNFsMKPR9EyMZeS9weJAEb&"
+        "oauth_version=1.0&status=Hello%20Ladies%20%2B%20Gentlemen%2C%20a%20"
+        "signed%20OAuth%20request%21"
+    ),
+    "expected_base_string": (
+        "POST&https%3A%2F%2Fapi.twitter.com%2F1.1%2Fstatuses%2Fupdate.json&"
+        "include_entities%3Dtrue%26oauth_consumer_key%3Dxvz1evFS4wEEPTGEFPHBog%26"
+        "oauth_nonce%3DkYjzVBB8Y0ZFabxSWbWovY3uYSQ2pTgmZeNu2VS4cg%26"
+        "oauth_signature_method%3DHMAC-SHA1%26oauth_timestamp%3D1318622958%26"
+        "oauth_token%3D370773112-GmHxMAgYyLbNEtIKZeRNFsMKPR9EyMZeS9weJAEb%26"
+        "oauth_version%3D1.0%26status%3DHello%2520Ladies%2520%252B%2520Gentlemen"
+        "%252C%2520a%2520signed%2520OAuth%2520request%2521"
+    ),
+    "expected_signature": "hCtSmYh+iHYCEqBWrE7C7hYmtUk=",
+}
+MOCK_TOKENS = {
+    "TW_CONSUMER_KEY": "xvz1evFS4wEEPTGEFPHBog",
+    "TW_ACCESS_TOKEN": "370773112-GmHxMAgYyLbNEtIKZeRNFsMKPR9EyMZeS9weJAEb",
+    "TW_CONSUMER_SECRET": "kAcSOqF21Fu85e7zjz7ZN2U4ZRhfV3WpwPAoE3Z7kBw",
+    "TW_ACCESS_SECRET": "LswwdoUaIvS8ltyTt5jkRh4J50vUPVVHtR2YPi5kE",
+}
+
+
+def test_key_collect() -> None:
+    client = UserAuthClient()
+    assert client.key_collect()
+
+
+def test_key_collect_raises() -> None:
+    client = UserAuthClient()
+    with patch.dict(os.environ):
+        del os.environ["TW_CONSUMER_KEY"]
+        with pytest.raises(KeyError):
+            client.key_collect()
+
+
+def test_generate_paramter_string() -> None:
+    client = UserAuthClient()
+    with patch.dict(os.environ, MOCK_TOKENS):
+        keys = client.key_collect()
+        keys["oauth_nonce"] = MOCK_VALUES["oauth_nonce"]
+        keys["oauth_timestamp"] = MOCK_VALUES["oauth_timestamp"]
+        parameter_string = client.generate_parameter_string(
+            fields={**keys, **MOCK_VALUES["fields"]}
+        )
+    assert parameter_string == MOCK_VALUES["expected_parameter_string"]
+
+
+def test_generate_signature_base_string() -> None:
+    client = UserAuthClient()
+    parameter_string = MOCK_VALUES["expected_parameter_string"]
+    base_string = client.generate_base_string(
+        method=MOCK_VALUES["method"],
+        route=MOCK_VALUES["route"],
+        parameter_string=parameter_string,
+    )
+    assert base_string == MOCK_VALUES["expected_base_string"]
+
+
+def test_generate_signature_string() -> None:
+    client = UserAuthClient()
+    base_string = MOCK_VALUES["expected_base_string"]
+    with patch.dict(os.environ, MOCK_TOKENS):
+        signature = client.generate_signature_string(base_string)
+    assert signature == MOCK_VALUES["expected_signature"]
+
+
+def test_generate_signature_string_raises() -> None:
+    client = UserAuthClient()
+    with patch.dict(os.environ):
+        del os.environ["TW_CONSUMER_SECRET"]
+        with pytest.raises(KeyError):
+            client.generate_signature_string("")
