564593a46fb6c265802a0756844a80b0bb5ad794
Preocts <preocts@gmail.com>
2021-12-05 15:40:08 -0500

    Update tests after refactors

--- tests/client_intrfc_test.py
+++ tests/client_intrfc_test.py
@@ -1,10 +1,13 @@
 from datetime import datetime
+from typing import NamedTuple
+from unittest.mock import MagicMock
 
 import pytest
 from twitterapiv2.client_intrfc import ClientIntrfc
+from twitterapiv2.exceptions import InvalidResponseError
+from twitterapiv2.exceptions import ThrottledError
+from twitterapiv2.model.httpresponse import HTTPResponse
 from twitterapiv2.model.responseheader import ResponseHeader
-
-MOCK_LAST_RESPONSE = ResponseHeader()
 
 
 def test_default_values() -> None:
@@ -15,12 +18,15 @@ def test_default_values() -> None:
 
 def test_properties() -> None:
     client = ClientIntrfc()
-    reset = datetime.utcfromtimestamp(1637818406)
-    mock_response = ResponseHeader()
-    mock_response.x_rate_limit_remaining = "10"
-    mock_response.x_rate_limit_reset = "1637818406"
+    mock_resp = HTTPResponse(None)
+    mock_headers = ResponseHeader()
+    mock_headers.x_rate_limit_remaining = "10"
+    mock_headers.x_rate_limit_reset = "1637818406"
+    mock_resp._response_headers = mock_headers
 
-    client.http.last_response = mock_response
+    reset = datetime.utcfromtimestamp(1637818406)
+
+    client._last_response = mock_resp
 
     assert client.limit_remaining == 10
     assert client.limit_reset == reset
@@ -50,3 +56,29 @@ def test_fetch_not_implemented() -> None:
     client = ClientIntrfc()
     with pytest.raises(NotImplementedError):
         client.fetch()
+
+
+class MockReponse(NamedTuple):
+    status: int = 200
+    body: str = ""
+    response_headers: MagicMock = MagicMock()
+
+
+def test_response_handling_429() -> None:
+    mock_resp = MockReponse(429, "")
+    client = ClientIntrfc()
+    with pytest.raises(ThrottledError):
+        client.raise_on_response("https://", mock_resp)
+
+
+def test_response_handling_401() -> None:
+    mock_resp = MockReponse(401, "")
+    client = ClientIntrfc()
+    with pytest.raises(InvalidResponseError):
+        client.raise_on_response("https://", mock_resp)
+
+
+def test_response_handing_200() -> None:
+    mock_resp = MockReponse(200, "")
+    client = ClientIntrfc()
+    client.raise_on_response("https://", mock_resp)
--- tests/http_test.py
+++ tests/http_test.py
@@ -1,71 +1,48 @@
 import json
-from typing import NamedTuple
 from unittest.mock import MagicMock
 from unittest.mock import patch
 
-import pytest
-import vcr
-from twitterapiv2.exceptions import InvalidResponseError
-from twitterapiv2.exceptions import ThrottledError
 from twitterapiv2.http import Http
 
 
-api_recorder = vcr.VCR(
-    filter_headers=["Authorization"],
-    record_mode="ONCE",
-    cassette_library_dir="tests/cassettes/http",
-)
+# api_recorder = vcr.VCR(
+#     filter_headers=["Authorization"],
+#     record_mode="ONCE",
+#     cassette_library_dir="tests/cassettes/http",
+# )
 
 
-class MockReponse(NamedTuple):
-    status: int
-    data: bytes
+# @api_recorder.use_cassette()
+# def test_last_response_headers() -> None:
+#     client = Http()
+#     fields = {"max_results": 10, "query": "test"}
+
+#     client.get("https://api.twitter.com/2/tweets/search/recent", fields)
+#     assert client.last_response
+#     assert client.last_response.x_rate_limit_remaining
+#     assert client.last_response.x_rate_limit_reset
+
+#     remaining = client.last_response.x_rate_limit_remaining
+#     reset = client.last_response.x_rate_limit_reset
+#     client.get("https://api.twitter.com/2/tweets/search/recent", fields)
+#     assert client.last_response
+#     assert client.last_response.x_rate_limit_remaining == str(int(remaining) - 1)
+#     assert client.last_response.x_rate_limit_reset == reset
 
 
-def test_data2dict() -> None:
+def test_mock_get() -> None:
+    data = json.dumps({"test": "mock"}).encode()
     client = Http()
-    good_data = b'{"test": "value"}'
-    bad_data = b"error data"
-    assert client.data2dict(good_data) == {"test": "value"}
-    assert client.data2dict(bad_data) == {"error": bad_data}
-
-
-def test_response_handling_429() -> None:
-    mock_resp = MockReponse(429, b"")
-    client = Http()
-    with pytest.raises(ThrottledError):
-        client.raise_on_response(mock_resp, "")
-
-
-def test_response_handling_401() -> None:
-    mock_resp = MockReponse(401, b"")
-    client = Http()
-    with pytest.raises(InvalidResponseError):
-        client.raise_on_response(mock_resp, "")
-
-
-def test_response_handing_200() -> None:
-    mock_resp = MockReponse(200, b"")
-    client = Http()
-    client.raise_on_response(mock_resp, "")
-
-
-@api_recorder.use_cassette()
-def test_last_response_headers() -> None:
-    client = Http()
-    fields = {"max_results": 10, "query": "test"}
-
-    client.get("https://api.twitter.com/2/tweets/search/recent", fields)
-    assert client.last_response
-    assert client.last_response.x_rate_limit_remaining
-    assert client.last_response.x_rate_limit_reset
-
-    remaining = client.last_response.x_rate_limit_remaining
-    reset = client.last_response.x_rate_limit_reset
-    client.get("https://api.twitter.com/2/tweets/search/recent", fields)
-    assert client.last_response
-    assert client.last_response.x_rate_limit_remaining == str(int(remaining) - 1)
-    assert client.last_response.x_rate_limit_reset == reset
+    resp = MagicMock(status=200, data=data)
+    with patch.object(client.http, "request", MagicMock(return_value=resp)) as post:
+        result = client.get("mockurl", {"payload": "test"}, {"header": "test"})
+        post.assert_called_once_with(
+            method="GET",
+            url="mockurl",
+            fields={"payload": "test"},
+            headers={"header": "test"},
+        )
+        assert result.json == {"test": "mock"}
 
 
 def test_mock_post() -> None:
@@ -75,9 +52,9 @@ def test_mock_post() -> None:
     with patch.object(client.http, "request", MagicMock(return_value=resp)) as post:
         result = client.post("mockurl", {"payload": "test"}, {"header": "test"})
         post.assert_called_once_with(
-            "POST",
-            "mockurl",
+            method="POST",
+            url="mockurl",
             body='{"payload": "test"}',
             headers={"header": "test"},
         )
-        assert result == {"test": "mock"}
+        assert result.json == {"test": "mock"}
