0def934693f85b8ab08a30213fce9c6f16e46c19
Preocts <preocts@gmail.com>
2022-03-15 17:51:34 -0400

    Annotations update

--- src/twitterapiv2/model/tweet_count/meta.py
+++ src/twitterapiv2/model/tweet_count/meta.py
@@ -1,5 +1,6 @@
+from __future__ import annotations
+
 from typing import Any
-from typing import Dict
 
 from twitterapiv2.model.base_model import BaseModel
 
@@ -9,7 +10,7 @@ class Meta(BaseModel):
     next_token: str  # Used with Academic Research Only
 
     @classmethod
-    def build_from(cls, data: Dict[str, Any]) -> "Meta":
+    def build_from(cls, data: dict[str, Any]) -> Meta:
         new = cls()
         new.total_tweet_count = data.get("total_tweet_count", 0)
         new.next_token = data.get("next_token", "")
