ae5cf2c3cacb5d9c5c0539fc856a2071e967e055
Preocts <preocts@preocts.com>
2023-01-13 23:42:42 -0500

    feat: refactor to use application_auth model

--- src/twitterapiv2/appauth_client.py
+++ src/twitterapiv2/appauth_client.py
@@ -6,69 +6,69 @@ https://developer.twitter.com/en/docs/authentication/oauth-2-0/application-only
 from __future__ import annotations
 
 import logging
-import os
 from base64 import b64encode
 from typing import Any
 from urllib import parse
 
 import httpx
+from twitterapiv2.model.application_auth import ApplicationAuth
 
 
 class AppAuthClient:
-    """
-    Authentication requires the following environment variables exist:
-        TW_CONSUMER_KEY
-        TW_CONSUMER_SECRET
 
-    A 'TW_BEARER_TOKEN' will be created in the environment on successful
-    authentication. This key should be stored securely and loaded to the
-    environment on subsequent calls. When this token already exists, the
-    request for a bearer token can be skipped.
-    """
+    logger = logging.getLogger(__name__)
 
     twitter_api = "https://api.twitter.com"
 
-    def __init__(self) -> None:
+    def __init__(self, authentication_keys: ApplicationAuth) -> None:
+        """Provide ApplicatoinAuth model for authentication."""
         self.http = httpx.Client()
-        self.log = logging.getLogger(__name__)
+        self._keys = authentication_keys
 
-    def encoded_credentials(self) -> str:
-        """Creates an encoded bear token credential string"""
-        key = os.getenv("TW_CONSUMER_KEY", None)
-        secret = os.getenv("TW_CONSUMER_SECRET", None)
+    @property
+    def consumer_key(self) -> str:
+        """Loaded consumer key."""
+        return self._keys.consumer_key
 
-        if key is None or secret is None:
+    @property
+    def consumer_secret(self) -> str:
+        """Loaded consumer secret."""
+        return self._keys.consumer_secret
+
+    @property
+    def consumer_bearer(self) -> str | None:
+        """Aquired bearer token, if any. Can be reused until revoked."""
+        return self._keys.consumer_bearer
+
+    def _encoded_credentials(self) -> str:
+        """Create encoded token credential string."""
+        if not self.consumer_key or not self.consumer_secret:
             raise KeyError("Missing one or both consumer environment variables!")
 
-        key = parse.quote_plus(key)
-        secret = parse.quote_plus(secret)
+        key = parse.quote_plus(self.consumer_key)
+        secret = parse.quote_plus(self.consumer_secret)
         union = ":".join([key, secret]).encode()
         return b64encode(union).decode()
 
-    def set_bearer_token(self) -> None:
-        """Fetches bearer token, setting it to `TW_BEARER_TOKEN` in environment"""
-        os.environ["TW_BEARER_TOKEN"] = self.fetch_bearer_token()
-        self.log.debug("Bearer token loaded to 'TW_BEARER_TOKEN'")
-
-    def fetch_bearer_token(self) -> str:
-        """Return bearer token for Twitter API v2"""
+    def _get_bearer_token(self) -> str:
+        """Get bearer token for Twitter API v2, uses provided if exists."""
         # Twitter does not frequently auto-expire bearer tokens. This will not
         # return a new (changed) bearer once one is granted until that token
         # is revoked.
-        self.log.debug("Requesting bearer token with consumer credentials...")
+        if self.consumer_bearer:
+            return self.consumer_bearer
+
+        self.logger.debug("Requesting bearer token with consumer credentials")
         url = self.twitter_api + "/oauth2/token"
         fields = {"grant_type": "client_credentials"}
 
         result = self._post_request(url=url, fields=fields)
 
-        if result.get("token_type", "") != "bearer":
-            self.log.error(result)
-            raise ValueError(f"Invalid token_type: '{result.get('token_type')}'")
+        if "token_type" not in result or "access_token" not in result:
+            self.logger.debug("Invalid response: %s", result)
+            raise ValueError("Unexpected Authentication response.")
 
-        if not result.get("access_token"):
-            self.log.error(result)
-            raise ValueError("No bearer token returned")
-
+        self._keys.consumer_bearer = result["access_token"]
         return result["access_token"]
 
     # def revoke_bearer_token(self) -> None:
@@ -90,12 +90,8 @@ class AppAuthClient:
         """Internal use: makes validate and invalidate calls, returns result"""
         headers = {
             "Content-Type": "applicaton/x-www-form-urlencoded;charset=UTF-8",
-            "Authorization": "Basic " + self.encoded_credentials(),
+            "Authorization": "Basic " + self._encoded_credentials(),
         }
-        resp = self.http.post(
-            url=url,
-            params=fields,
-            headers=headers,
-        )
+        resp = self.http.post(url=url, params=fields, headers=headers)
 
         return resp.json() if resp.is_success else {}
