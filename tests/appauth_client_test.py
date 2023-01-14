2d7c572ca277f87289500307909b98bde3d0be3d
Preocts <preocts@preocts.com>
2023-01-13 23:53:44 -0500

    chore: adjust tests for refactored class

--- tests/appauth_client_test.py
+++ tests/appauth_client_test.py
@@ -1,17 +1,18 @@
 from __future__ import annotations
 
-import os
 from collections.abc import Generator
 from unittest.mock import patch
 
 import pytest
 from twitterapiv2.appauth_client import AppAuthClient
+from twitterapiv2.model.application_auth import ApplicationAuth
 
 from tests.fixtures.clientmocker import ClientMocker
 
 MOCK_KEY = "xvz1evFS4wEEPTGEFPHBog"
 MOCK_SECRET = "L8qq9PZyRg6ieKGEKhZolGC0vJWLw8iEJ88DRdyOg"
 MOCK_CRED = "eHZ6MWV2RlM0d0VFUFRHRUZQSEJvZzpMOHFxOVBaeVJnNmllS0dFS2hab2xHQzB2SldMdzhpRUo4OERSZHlPZw=="  # noqa: E501
+MOCK_BEARER = "AAAAAAAAAAAAAAAAAAAAAP84VgEAAAAAQY8EsmI4GGhUif1WbMYnbLfrOyA%3DOY35WqNFisAnDtjp08qaXjYB2n6xncWp3pdoAOPPjx6gUL2Y83"  # noqa: E501
 
 MOCK_RESP = '{"token_type":"bearer","access_token":"AAAAAAAAAAAAAAAAAAAAAP84VgEAAAAAQY8EsmI4GGhUif1WbMYnbLfrOyA%3DOY35WqNFisAnDtjp08qaXjYB2n6xncWp3pdoAOPPjx6gUL2Y83"}'  # noqa: E501
 BAD_REQUEST = '{"errors":[{"code":99,"message":"Unable to verify your credentials","label":"authenticity_token_error"}]}'  # noqa: 501
@@ -20,55 +21,42 @@ BAD_RESPONSE = '{"token_type":"bearer"}'
 
 @pytest.fixture
 def client() -> Generator[AppAuthClient, None, None]:
-    appclient = AppAuthClient()
+    appclient = AppAuthClient(ApplicationAuth(MOCK_KEY, MOCK_SECRET))
     with patch.object(appclient, "http", ClientMocker()):
         yield appclient
 
 
-def test_encoded_credentials() -> None:
-    client = AppAuthClient()
-    env = {
-        "TW_CONSUMER_KEY": MOCK_KEY,
-        "TW_CONSUMER_SECRET": MOCK_SECRET,
-    }
-    with patch.dict(os.environ, env):
-        result = client.encoded_credentials()
-        assert result == MOCK_CRED
+def test_encoded_credentials(client: AppAuthClient) -> None:
+    result = client._encoded_credentials()
+    assert result == MOCK_CRED
 
 
-def test_require_environ_vars() -> None:
-    client = AppAuthClient()
-    os.environ.pop("TW_CONSUMER_KEY", None)
+def test_required_applicationauth_key(client: AppAuthClient) -> None:
+    client._keys.consumer_key = ""
     with pytest.raises(KeyError):
-        client.encoded_credentials()
+        client._encoded_credentials()
 
-    os.environ["TW_CONSUMER_KEY"] = "Mock"
-    os.environ.pop("TW_CONSUMER_SECRET", None)
+
+def test_required_applicationauth_secret(client: AppAuthClient) -> None:
+    client._keys.consumer_secret = ""
     with pytest.raises(KeyError):
-        client.encoded_credentials()
+        client._encoded_credentials()
 
 
-def test_set_bearer_token(client: AppAuthClient) -> None:
+def test_get_bearer_token(client: AppAuthClient) -> None:
     client.http.add_response(MOCK_RESP, {}, 200, client.twitter_api + "/oauth2/token")
 
-    client.set_bearer_token()
+    result = client._get_bearer_token()
 
-    assert os.getenv("TW_BEARER_TOKEN") is not None
-
-
-def test_fetch_bearer_token(client: AppAuthClient) -> None:
-    client.http.add_response(MOCK_RESP, {}, 200, client.twitter_api + "/oauth2/token")
-
-    client.fetch_bearer_token()
-
-    assert os.getenv("TW_BEARER_TOKEN") is None
+    assert result == MOCK_BEARER
+    assert client.consumer_bearer == MOCK_BEARER
 
 
 def test_invalid_bearer_request(client: AppAuthClient) -> None:
     client.http.add_response(BAD_REQUEST, {}, 403, client.twitter_api + "/oauth2/token")
 
     with pytest.raises(ValueError):
-        client.set_bearer_token()
+        client._get_bearer_token()
 
 
 def test_invalid_bearer_response(client: AppAuthClient) -> None:
@@ -77,4 +65,12 @@ def test_invalid_bearer_response(client: AppAuthClient) -> None:
     )
 
     with pytest.raises(ValueError):
-        client.set_bearer_token()
+        client._get_bearer_token()
+
+
+def test_get_bearer_token_with_existing(client: AppAuthClient) -> None:
+    client._keys.consumer_bearer = "mock"
+
+    result = client._get_bearer_token()
+
+    assert result == "mock"
