142d73e38241e86da30a5642fc17eee487581274
Preocts <preocts@preocts.com>
2023-01-16 20:47:00 -0500

    chore: refine abc

--- src/twitterapiv2/_auth_client.py
+++ src/twitterapiv2/_auth_client.py
@@ -1,12 +1,16 @@
 from __future__ import annotations
 
 import abc
+from typing import Any
 
 
 class AuthClient(abc.ABC):
-    """Abstract of all auth clients."""
+    """Abstract for all auth clients."""
+
+    @abc.abstractmethod
+    def __init__(self, auth_model: Any, scopes: list[str]) -> None:
+        raise NotImplementedError()
 
     @abc.abstractmethod
     def get_bearer(self) -> str | None:
-        """Aquire berer token from Twitter, or return current."""
         raise NotImplementedError()
